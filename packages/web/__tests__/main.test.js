f45ba5cfff32efcabb768972b4fea7f689a0a3bd
Marius Reimer <reime005@gmail.com>
2021-01-09 19:38:17 +0100

    fix: test

--- packages/web/__tests__/main.test.js
+++ packages/web/__tests__/main.test.js
@@ -56,15 +56,15 @@ test('navigate to a blog article', async () => {
     e => e.textContent,
   );
 
-  await page.screenshot({ path: `screenshots/${Date.now() / 1000}_0.png` });
+  await page.screenshot({ path: `screenshots/${Date.now() / 1000}_${listHeaderTitle}_0.png` });
 
-  await page.click(`data-testid=blog-header-title`);
+  await page.click(`data-testid=blog-header`);
 
-  await page.screenshot({ path: `screenshots/${Date.now() / 1000}_1.png` });
+  await page.screenshot({ path: `screenshots/${Date.now() / 1000}_${listHeaderTitle}_1.png` });
 
   await page.waitForSelector('data-testid=blog-title');
 
-  await page.screenshot({ path: `screenshots/${Date.now() / 1000}_2.png` });
+  await page.screenshot({ path: `screenshots/${Date.now() / 1000}_${listHeaderTitle}_2.png` });
 
   const headerTitle = await page.$eval(
     'data-testid=blog-title',
